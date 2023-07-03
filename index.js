395738dc824f6e31864d6f2222781493a463a3a4
HuongMT98 <134026872+HuongMT98@users.noreply.github.com>
2023-07-03 22:06:15 +0700

    Main

---
+++ index.js
@@ -0,0 +1,38 @@
+// //♦️♦️♦️♦️♦️♦️♦️♦️♦️♦️♦️♦️♦️♦️♦️♦️
+// //♦️♦️♦️♦️♦️♦️♦️♦️♦️♦️♦️♦️♦️♦️♦️♦️
+
+// // Gán tên Biến
+// var hoTen = 'Mai Thanh Hưởng'
+// alert(hoTen)
+
+// //♦️♦️♦️♦️♦️♦️♦️♦️♦️♦️♦️♦️♦️♦️♦️♦️
+// //♦️♦️♦️♦️♦️♦️♦️♦️♦️♦️♦️♦️♦️♦️♦️♦️
+
+// // Hàm Build-in funtion
+//♣️♣️♣️♣️♣️♣️♣️♣️♣️♣️♣️♣️♣️♣️♣️
+// 1. alert: tạo ra pop-up cảnh báo người dùng
+//♣️♣️♣️♣️♣️♣️♣️♣️♣️♣️♣️♣️♣️♣️♣️
+// 2.Console : In dòng hông báo trên tag console trên trình duyệt
+//♣️♣️♣️♣️♣️♣️♣️♣️♣️♣️♣️♣️♣️♣️♣️
+// console.log
+// console.error
+// console.warn
+//♣️♣️♣️♣️♣️♣️♣️♣️♣️♣️♣️♣️♣️♣️♣️
+// 3. Confirm : Tạo ra pop-up xác nhận người dùng
+// confirm('Xác nhận bạn là người đăng nhập')
+//♣️♣️♣️♣️♣️♣️♣️♣️♣️♣️♣️♣️♣️♣️♣️
+// 4. Prompt : tạo ra pop-up cho người dùng nhập vào
+// prompt('Xác nhận bạn không phải robot, Hãy điền YES')
+//♣️♣️♣️♣️♣️♣️♣️♣️♣️♣️♣️♣️♣️♣️♣️
+// 5. Set timeout: Dùng truyền 1 funtion và thời gian delay để chạy sau này, đơn vị mili giây
+// setTimeout(function () {
+//     alert('Thông Báo, bạn đã truy cập vào website của chúng tôi')
+// }, 3000)
+//♣️♣️♣️♣️♣️♣️♣️♣️♣️♣️♣️♣️♣️♣️♣️
+// 6. setInterval dùng để truyền 1 funtion vào sẽ được lặp lại sau một khoảng thời gian đơn vị milli giây
+// setInterval(function () {
+//     alert('tên Bạn')
+// }, 2000)
+
+// //♦️♦️♦️♦️♦️♦️♦️♦️♦️♦️♦️♦️♦️♦️♦️♦️
+// //♦️♦️♦️♦️♦️♦️♦️♦️♦️♦️♦️♦️♦️♦️♦️♦️
