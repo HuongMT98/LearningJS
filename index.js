c725615a57a90486c7c6bc5738c35fcc68921ea5
HuongMT98 <134026872+HuongMT98@users.noreply.github.com>
2023-07-04 12:39:34 +0700

    Funtion

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+//! ------------------------
 // ! Gán tên Biến
 // var hoTen = 'Mai Thanh Hưởng'
 // alert(hoTen)
 
+//! ------------------------
 // ! Hàm Build-in funtion
 // * 1. alert: tạo ra pop-up cảnh báo người dùng
 // alert('Chào cậu')
@@ -27,7 +29,7 @@
 //     console.log('Đây là số:' + Math.random)
 // }, 2000)
 
-
+//! ------------------------
 // ! If - Else : Điều kiện nếu thì
 // var a = 3 + 6
 // if (a < 2) {
@@ -36,6 +38,7 @@
 //     alert('Điều kiện sai')
 // }
 
+//! ------------------------
 // ! Toán tử Logic
 // * 1: && và và, đây là toán tử bắt buộc mọi điều kiện phải đúng thì mới đúng
 // var a = 1
@@ -64,6 +67,7 @@
 //     alert('Điều kiện sai')
 // }
 
+//! ------------------------
 // ! Kiểu dữ liệu trong JS
 // * 1: Number: Số học có thể là số âm hay dương, số thực với các chấm, không nhất thiết phải có dấu.
 // var a = 1
@@ -107,10 +111,11 @@
 
 //  *10: Cách kiểm tra kiểu dữ liệu dùng typeof
 
-var a = 1
-console.log((typeof a));
-console.log((typeof 'a'))
+// var a = 1
+// console.log((typeof a));
+// console.log((typeof 'a'))
 
+//! ------------------------
 // ! toán tử so sánh kiểu dữ liệu và giá trị
 //* So sánh giá trị tuyệt đối
 // var a = 1
@@ -122,9 +127,212 @@ console.log((typeof 'a'))
 // var b = 5
 // console.log((a !== b));
 
+//! ------------------------
+//! Chuỗi
+//* 1: tạo chuỗi
+// var fullname = 'Thanh Hưởng'
+// var age = '25'
+// console.log(fullname + age)
 
-//! Câu điều kiện và phép so sánh
-//* 
+// * 2:Xem độ dài chuỗi
+// var fullname = 'Thanh Hưởng'
+// var age = '25'
+// console.log(fullname.length + age.length)
+
+// * 3: Dùng dấu \ để hiển thị '' hoặc "" hoặc \ trong chuỗi
+// var str = "\'\"\\hello world\\" // output là \'\"\hello world\'
+// console.log("str:", str);
+
+//! ------------------------
+//! Làm việc với chuỗi
+// *1 length : Độ dài chuỗi
+// var name = "thanhhuong";
+// console.log('Độ dài chuỗi của ' + name + ' là ' + name.length + '.');
+
+// *2 findindex : Tìm ký tự trong chuỗi trong chuỗi
+// var text = "Thanh Huong";
+// let index = text.indexOf("u");   // Output: 0 (với ký tự u xuất hiện lần đầu tiên).
+// if (index !== -1) {                  // Nếu không gặp, in ra
+//     console.log(`Từ vựng 'u' được tìm thấy ở vị trí ${index}.`);
+// }
+
+// *3 Cut string : Cắt chuỗi
+// const myString = "<NAME>";
+// console.log(`${myString}`);
+// console.log(`${myString}`.slice(4))    // cắt chuỗi vị trí 4
+// console.log(`${myString}`.substring(6, -7))
+
+// *4 Replace ghi đè chuỗi
+// var a = 'Hoc JS tai F8'
+// console.log(a.replace('JS', 'Javascript'))
+// ? --> Thay thế JS thành Javascript
+
+// var a = 'Hoc JS JS JS tai F8'
+// console.log(a.replace(/JS/g, 'Javascript'))
+// ? --> Thay thế toàn bộ JS thành Javascript
+
+// *5 Convert uppercase thành chữ hoa
+// var a = 'Hoc JS JS JS tai F8'
+// console.log(a.toUpperCase())
+
+// *6 Convert to lowercase thành chữ thường
+// var a = 'Hoc JS JS JS tai F8'
+// console.log(a.toLowerCase());
+
+// *7 Strim Cắt bỏ khoảng trắng thừa 2 đầu
+// var str = ' Hoc Js ';
+// console.log(str);           // output: " Hoc Js "
+// console.log(str.trim());      // output: "HocJs"
+
+// *8 Split cắt chuỗi
+// var traiCay = 'Apple';
+// console.log(traiCay.split(''))
+
+//! ------------------------
+//! Kiểu số Number và làm việc với kiểu số Number
+// * 1 Kiểu To String : biến kiểu số Number thành chuỗi String
+// var age = 24;
+// var PI = 3.14
+
+// var myString = PI.toString()
+// console.log(myString)
+
+// *2 Kiểu to Fixed : Làm tròn số
+// var pi = 3.14343434
+// console.log(pi.toFixed())
+
+//! ------------------------
+//! Mảng: Array
+// *1 : Tạo mảng
+// var fruits = [
+//     'apple',
+//     'banana',
+//     'orange'
+// ];
+// console.log("Mảng Fruits:", fruits);
+
+// *2 : Truy xuất mảng
+// console.log(fruits[0])     // lấy ra phần tử đầu tiên
+// console.log(fruits[2])      // lấy ra phần tử cuối cùng
+
+
+//  *3 : Thêm vào Mảng
+// fruits.push('grape')   // thêm item vô cuối của array
+// console.log("Push grape", fruits);
+
+// *4 Xóa khỏi mảng
+// fruits.pop();    // Xóa phần tử cuối cùng
+// console.log("Xóa phần tử cuối cùng trong mảnh ", fruits);
+
+// *5 Cắt mảng Array
+// let arr = ['one', 'two', 'three'];
+// arr.splice(1, 1)
+// console.log(arr)
+
+// *6 Xóa phần tử đầu mảng Shift
+// let myArray = ['Nigga', "Hello World!", 'WOW'];
+// console.log(myArray.shift())
+// console.log(myArray)
+
+// *7 Thêm phần tử đầu mảng Array unShift
+// let newArr = ["apple", "banana"];
+// console.log(newArr)
+// newArr.unshift("orange"); ---> thêm orange vào mảng
+// console.log(`Unshift: ${JSON.stringify(newArr)}`);
+// console.log(newArr)
+
+// *8 Xóa phần tử trong mảng Splice
+// let numbers = [20, 30, 40];
+// numbers.splice(1, 1);   // xóa số 30 ra và chỉ 1 số (1,1)
+// console.log('Splice:', JSON.stringify(numbers));
+
+// *9 Nối 2 mảng thành 1 mảng
+// let arrayOne = [1, 2, 3],
+//     arrayTwo = [4, 5, 6]
+// console.log([arrayOne, arrayTwo])
+// console.log(...[...arrayOne, ...arrayTwo]);
+
+// *10 Slicing array
+// let fruits = ["Banana", "Orange", "Mango", "Apple", "Green Apple"]
+// console.log(fruits.slice(1)) // ---> Bắt đầu cắt mảng từ vị trí chỉ định
+
+
+//! ------------------------
+//! Hàm : Funtion là một khối mã chức năng và không tự chạy đến khi được gọi
+// *1 Tạo hàm Funtion
+// function sayHello() {
+//     return 'hello world';
+// }
+// console.log(sayHello())
+
+// //✨✨✨✨✨✨✨✨✨✨✨
+// function showDiablog() {
+//     alert('Hello world')
+// }
+// showDiablog() //---> Chỉ đến khi gọi mới thực thi
+
+// *2 Tham số trong hàm Funtion
+// function writeLof(message) {
+//     console.log(message)
+// }
+// writeLof('Tham số thứ 1')
+
+// //✨✨✨✨✨✨✨✨✨✨✨
+// function writeLof(message, message1) {
+//     if (message) {
+//         console.log(message)
+//     } if (message1) {
+//         console.log(message1)
+//     }
+// }
+// writeLof('Đây là tham số thứ 1', 'Đây là tham số thứ 2')
+
+
+// *3 Return trong Funtion
+// var isComfirm = confirm('Bạn đủ tuổi chưa?')
+// console.log(isComfirm)
+
+// //✨✨✨✨✨✨✨✨✨✨✨
+// function phepCong(a, b) {
+//     return a + b; // ---> Bên dưới hàm return sẽ bị bỏ qua
+// }
+// var result = phepCong(2, 8)
+// console.log(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
