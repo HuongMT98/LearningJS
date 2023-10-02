eb0d61efdcc314a13ebbd8905e82ec23c2481169
HuongMT98 <134026872+HuongMT98@users.noreply.github.com>
2023-10-02 09:38:34 +0700

    Toán tử

---
+++ JS/lesson_1.js
@@ -0,0 +1,122 @@
+////❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️
+////! KHAI BÁO BIẾN
+////* ➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖
+
+// var fullName = 'Mai Thanh Hưởng'
+// var age = 25
+
+// console.log(fullName + ', tuổi: ' + age)
+
+////❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️
+////!   THUẬT NGỮ BUILD-IN
+////* ➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖
+/*
+    1:  Alert:hiển thi pop-up trên màn hình
+    2:  Console : Dùng để làm việc với cửa sổ console
+    3:  Confirm : Dùng để tạo ra pop-up xác nhận từ người dùng
+    4:  Promt : Dùng để lấy giá trị người dùng nhập từ pop-up
+    5:  Set timeout : Dùng để thực hiện trong khoảng thời gian
+    6   Set Interval : Dùng để thực hiện trong khoản thời gian và liên tục
+*/
+////✨✨✨✨✨✨✨✨
+////* Alert:
+// alert("Test hàm alert")
+////✨✨✨✨✨✨✨✨
+////*Confirm:
+// confirm('Xác nhận bạn đủ tuổi')
+////✨✨✨✨✨✨✨✨
+////*Promt:
+// prompt("Nhập tuổi bạn để truy cập: ")
+////✨✨✨✨✨✨✨✨
+////*Settimeout:
+// setTimeout(() => {
+//     confirm('Test hàm setTimeout')
+// }, 2000);
+////✨✨✨✨✨✨✨✨
+////*setInterval:
+// setInterval(() => {
+//     console.log('Test hàm Interval')
+// }, 2000);
+
+////❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️
+////!   TOÁN TỬ
+
+////💎💎💎💎💎💎💎💎💎💎💎💎💎💎
+////? Toán tử số học: + - * / % ** ++ --
+
+// ////✨✨✨✨✨✨✨✨
+// //*Toán tử cộng
+// var a = 7
+// var b = 2
+// var c = a + b
+// console.log("Toán tử cộng là: " + c)
+
+// //✨✨✨✨✨✨✨✨
+// //*Toán tử trừ
+// var a = 8
+// var b = 5
+// var c = a - b
+// console.log("Toán tử trừ là: " + c)
+
+// //✨✨✨✨✨✨✨✨
+// //*Toán tử nhân
+// var a = 9;
+// var b = 4;
+// var c = a * b
+// console.log("Toán tử nhân là: " + c)
+
+// //✨✨✨✨✨✨✨✨
+// //*Toán tử chia
+// var a = 16;
+// var b = 3;
+// var c = Math.floor(a / b)
+// console.log("Toán tử chia là: " + c)
+
+// //✨✨✨✨✨✨✨✨
+// //*Lũy thừa
+// var a = 2
+// var b = 5
+// var c = a ** b
+// console.log(`Lũy thừa của ${b} lần với ${c}`)
+
+// //✨✨✨✨✨✨✨✨
+// //*Chia lấy số dư
+// var a = 10
+// var b = 3
+// var c = a % b
+// console.log("Chia lấy số dư: " + c)
+
+// //✨✨✨✨✨✨✨✨
+// //*Tăng 1 giá trị số
+// var a = 10
+// a++
+// console.log("Tăng 1 giá trị số: " + a)
+
+
+// //✨✨✨✨✨✨✨✨
+// var a = 10
+// a--
+// console.log('Giảm 1 giá trị số:' + a);
+
+////💎💎💎💎💎💎💎💎💎💎💎💎💎💎
+////? Toán tử gán = += -= *= /= **=
+// let a = 10
+// console.log("Toán tử gán = : " + a)
+
+// a += 8
+// console.log("Toán tử gán += : " + a)
+
+// a -= 7
+// console.log("Toán tử gán -= : " + a)
+
+// a *= 3
+// console.log("Toán tử gán *= : " + a)
+
+// a /= 9
+// console.log("Toán tử gán /= : " + a)
+
+// a **= 4
+// console.log("Toán tử gán **=: " + a)
+
+
+
