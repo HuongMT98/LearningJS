7c80aa5fbfdedbd72a0562251c60e6ef9515b8f8
HuongMT98 <134026872+HuongMT98@users.noreply.github.com>
2023-10-19 08:03:33 +0700

    Object Prototype

--- JS/lesson_3.js
+++ JS/lesson_3.js
@@ -60,13 +60,158 @@
 ////❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️
 ////! SỐ TRONG JS
 ////✨✨✨✨✨✨✨✨
-////? 1: Tạo biến
-let num1 = 10;
-let pi = 3.14
+// ////? 1: Tạo biến
+// let num1 = 10;
+// let pi = 3.14
 
-////? 2: Kiểm tra datatype
-console.log(typeof num1)
+// ////? 2: Kiểm tra datatype
+// console.log(typeof num1)
+// console.log(typeof pi)
 
 
-////✨✨✨✨✨✨✨✨
-////? Làm việc với số trong js (Number Method)
+// ////✨✨✨✨✨✨✨✨
+// ////? Làm việc với số trong js (Number Method JS)
+// ////* To String: Biến số thành chuỗi
+// let numberDemo = 1000
+// console.log(numberDemo.toString())
+
+// ////* To Fixed: Làm tròn số thập phân
+// let fixedNum = 123.4567
+// console.log(fixedNum.toFixed())
+
+
+
+// ////❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️
+// ////! MẢNG TRONG JS
+// ////? 1: Tạo mảng
+// let myArray = [
+//     'JS',
+//     'Rudy',
+//     'PHP',
+//     null,
+//     undefined,
+//     function () {
+
+//     },
+// ]
+// console.log(myArray)
+
+
+////💎💎💎💎💎💎💎💎💎💎💎💎💎💎
+// ////? 2: Truy xuất mảng
+// console.log(myArray[1]);
+// console.log(myArray.length)
+
+
+////💎💎💎💎💎💎💎💎💎💎💎💎💎💎
+////? 3: Làm việc với mảng
+// var languages = [
+//     "JavaScript",
+//     "Python",
+//     "Java"
+// ]
+////💎💎💎💎💎💎💎💎💎💎💎💎💎💎
+////* To string: chuyễn array sang chuỗi
+// console.log(languages.toString())
+
+////💎💎💎💎💎💎💎💎💎💎💎💎💎💎
+////* Join: gộp các phần tử của mảng thành 1 chuỗi
+// console.log(languages.join(" - "))
+
+////💎💎💎💎💎💎💎💎💎💎💎💎💎💎
+////* Pop: xóa phần tử cuối mảng và trả về phần tử đã xóa
+// console.log(languages.pop());
+
+////💎💎💎💎💎💎💎💎💎💎💎💎💎💎
+////* Push: thêm vào mảng một vài phần tử
+// console.log(languages.push('Dart', 'C#'))
+// console.log(languages)
+
+////💎💎💎💎💎💎💎💎💎💎💎💎💎💎
+////* Shift: Xóa phần tử đầu mảng và trả về
+// console.log(languages.shift())
+
+////💎💎💎💎💎💎💎💎💎💎💎💎💎💎
+////* Unshift: Thêm phần tử vào đầu mảng
+// console.log(languages.unshift('C++'))
+// console.log(languages)
+
+////💎💎💎💎💎💎💎💎💎💎💎💎💎💎
+////*Splicing: Xóa phần tử vào mảng, chèn phần tử mới vào mảng
+// console.log(languages.splice(3));
+// //!--> Xóa mảng thứ 3
+// languages.splice(1, 0, 'Dart')
+// console.log(languages)
+// //!--> Thêm mảng vào vị trí 1, không xóa phần tử nào, và thêm Dart
+
+////💎💎💎💎💎💎💎💎💎💎💎💎💎💎
+////*Concat: Nối mảng
+// var languages1 = ['C#', 'C++']
+// console.log(languages.concat(languages1))
+
+////💎💎💎💎💎💎💎💎💎💎💎💎💎💎
+////*Slicing: Cắt một hoặc toàn bộ element
+// console.log(languages.slice(1))
+
+
+
+// ////❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️❄️
+// ////! OBJECT TRONG JS
+// ////✨✨✨✨✨✨✨✨
+////* Tạo đối tượng
+
+// let myInfo = {
+//     name: 'Hưởng',
+//     age: 25,
+//     adress: 'Kiên Giang',
+//     getName: function () {
+//         return this.name
+//     }
+// }
+// myInfo.email = "mthanhhuong85@gmail.com"////! --> Thêm Object
+// console.log(myInfo)
+
+// console.log(myInfo.name) //!--> Lấy thông tin
+// console.log(myInfo)
+
+// delete myInfo.age////!-->Xóa object
+// console.log(myInfo)
+
+
+
+// // ////✨✨✨✨✨✨✨✨
+// ////* Xây dựng đối tượng Object Contructor
+// function User(firstName, lastName, avatar) {
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//     this.avatar = avatar;
+// }
+
+// let tacGia = new User('Mai', 'Thanh Hưởng', 'Avatar')
+// let hocVien = new User('Trần', 'Trung', 'Avatar')
+
+// console.log(tacGia)
+// console.log(hocVien)
+
+
+
+// // ////✨✨✨✨✨✨✨✨
+// ////* Object prototype?: Nguyên mẫu tạo đối tượng dùng để tạo thêm thuộc tính bên ngoài 1 function
+// function User(firstName, lastName, avatar) {
+//     this.firstName = firstName;
+//     this.lastName = lastName;
+//     this.avatar = avatar;
+// }
+
+// User.prototype.className = 'Test prototype'////!--> Đây là cách dùng prototype để thêm thuộc tính
+
+// User.prototype.getClassName = function () {
+//     return this.className
+// }////!--> Và có thể dùng cho một phương thức (Function)
+
+
+// let tacGia1 = new User('Mai', 'Thanh Hưởng', 'Avatar')
+// let hocVien1 = new User('Trần', 'Trung', 'Avatar')
+
+
+// console.log(hocVien1.className)
