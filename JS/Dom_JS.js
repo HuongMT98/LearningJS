1ae8d8bd63ef80e9e1e91280d1ca58f1aea2730c
HuongMT98 <134026872+HuongMT98@users.noreply.github.com>
2023-10-27 12:21:30 +0700

    Học Về Dom

--- JS/Dom_JS.js
+++ JS/Dom_JS.js
@@ -1,7 +1,46 @@
 /*       
                         Tài liệu DOM: https://www.w3schools.com/js/js_htmldom.asp
+
+                        DOM chỉ có 3 thành phần:
+                        Element
+                        Atribute
+                        Text
 */
 
+let h2Node = document.getElementById('dom')
+
+
+let mangNumber = [
+    1, 2, 3, 5, 8, 7,
+]
+
+
+// const ${ 1: TênBiến } = ${ 2: TênMảngCha }.reduce((acc, cur) => {
+//     return [...acc, ...cur.${ 3: TênMảngCon }];
+// }, []);
+
+
+
+
+
+
+
+let mangHaiChieu = [
+    {
+        topic: "ReactJS",
+        posts: [
+            { postID: "id1", title: "title1" },
+            { postID: "id2", title: "title2" },
+        ],
+    },
+    {
+        topic: "Vue.js",
+        posts: [
+            { postID: "id3", title: "title3" },
+            { postID: "id4", title: "title4" },
+        ],
+    },
+]
 
 
 
