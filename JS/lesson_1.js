d024f8d72a900384d498e5f11b24df7aa64ee893
HuongMT98 <134026872+HuongMT98@users.noreply.github.com>
2024-01-18 09:04:06 +0700

    callback

--- JS/lesson_1.js
+++ JS/lesson_1.js
@@ -1,10 +1,650 @@
-//1: KHAI BÁO BIẾN TRONG JS
+//⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️
+//! 1: KHAI BÁO BIẾN TRONG JS
+//⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️
 
-// Ví dụ 1
-var name = 'Hưởng'
-let age = 25
-const gender = "Nam"
-let address = 'Kiên Giang'
+//? ∅∅∅∅∅∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∅∅∅∅∅
+//? Ví dụ 1
 
-console.log('1: Khai báo biến : ' + 'Tên ' + name + ' Tuổi ' + age + ' Giới tính ' + gender + ' Địa chỉ ' + address)
+// var name = 'Hưởng'
+// let age = 25
+// const gender = "Nam"
+// let address = 'Kiên Giang'
+
+// console.log('Tên ' + name + ' Tuổi ' + age + ' Giới tính ' + gender + ' Địa chỉ ' + address)
+
+
+//? ∅∅∅∅∅∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∅∅∅∅∅
+//? Ví dụ 2
+
+// var name1 = "John";
+// console.log("Xin chào, " + name1 + "!");
+
+
+//? ∅∅∅∅∅∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∅∅∅∅∅
+//? Ví dụ 3
+
+// var name2 = "John";
+// var age2 = 25;
+// var isStudent2 = true;
+
+// console.log("Tên: " + name2);
+// console.log("Tuổi: " + age2);
+// console.log("Là sinh viên: " + isStudent2);
+
+
+//? ∅∅∅∅∅∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∅∅∅∅∅
+//? Ví dụ 4
+
+// var a = 5;
+// var b = 3;
+// var sum = a + b;
+// var difference = a - b;
+// var product = a * b;
+// var quotient = a / b;
+
+// console.log("Tổng: " + sum);
+// console.log("Hiệu: " + difference);
+// console.log("Tích: " + product);
+// console.log("Thương: " + quotient);
+
+
+//? ∅∅∅∅∅∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∅∅∅∅∅
+//? Ví dụ 5
+
+// var width = 10;
+// var height = 5;
+// var area = width * height;
+
+// console.log("Diện tích hình chữ nhật là: " + area);
+
+
+
+
+//⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️
+//! 2 Comment trong JS
+//⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️
+
+
+// let name = "Hưởng"
+// let age = 26
+/* Đây là comment trong js */
+
+
+
+
+
+
+//⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️
+//! 3: BUILT-IN JUNCTION TRONG JS: Hàm xây dựng sẳn trong JS
+//* Alert
+//* Console
+//* Prompt
+//* Confirm
+//* Set timeout
+//* Set Interval
+//⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️
+//? ∅∅∅∅∅∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∅∅∅∅∅
+//? Alert
+
+// var name = 'Hưởng'
+// var age = 25
+// alert('Xin chào tôi tên là: ' + name + age + ' tuổi')
+
+
+//? ∅∅∅∅∅∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∅∅∅∅∅
+//? Console
+
+// console.log
+// console.error
+// console.warn
+
+
+//? ∅∅∅∅∅∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∅∅∅∅∅
+//? Promt
+
+// prompt('Xác nhận bạn lớn hơn 18 tuổi: Nhập OK')
+
+
+//? ∅∅∅∅∅∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∅∅∅∅∅
+//? Confirm
+
+// confirm('Xác nhận bạn là con người')
+
+
+//? ∅∅∅∅∅∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∅∅∅∅∅
+//? setTimeout
+
+// setTimeout(() => {
+//     alert('Thông báo')
+// }, 2000);
+
+
+//? ∅∅∅∅∅∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∅∅∅∅∅
+//? setInterval
+
+// setInterval(function () {
+//     console.log('Hello Guy ' + Math.random())
+// }, 1000);
+
+
+
+//⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️
+//! 4: TOÁN TỬ SỐ HỌC TRONG JS
+/*
+
+    + Cộng
+    - Trừ
+    * Nhân
+    / Chia (phép nhân)
+    % Lấy phần số tròn
+    ** Lũy thừa
+
+*/
+//⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️
+
+// var a = 1
+// var b = 3
+// var c = a + b
+// console.log("Toán tử cộng " + c)
+// var d = b - a
+// console.log("Toán tử trừ " + d)
+// var e = a * b
+// console.log("Toán tử nhân " + e)
+// var f = b / a
+// console.log("Phép chia " + f)
+// var g = b % a
+// console.log("Lấy phần số tròn " + g)
+// var h = a ** b
+// console.log("Luỹ thừa " + h)
+
+
+
+
+//⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️
+//! 5: TOÁN TỬ GÁN
+/*
+=       x = y
++=      x += y
+-=      x -= y
+*=      x *= y
+/=      x /= y
+**=     x **= y
+
+*/
+//⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️
+//? ∅∅∅∅∅∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∅∅∅∅∅
+//? Toán tử +=
+
+// var a = 1
+// a += 2
+// console.log(a)
+
+
+//? ∅∅∅∅∅∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∅∅∅∅∅
+//? Toán tử -=
+// var b = 2
+// b -= 2
+// console.log(b)
+
+
+//? ∅∅∅∅∅∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∅∅∅∅∅
+//? Toán tử *=
+// var c = 3;
+// var d = 4;
+// c *= d;
+// console.log(c)
+
+
+//? ∅∅∅∅∅∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∅∅∅∅∅
+//? Toán tử /=
+// var e = 6
+// var g = 3
+// e /= g
+// console.log(e)
+
+
+//? ∅∅∅∅∅∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∅∅∅∅∅
+//? Toán tử **=
+// var f = 2
+// f **= 3
+// console.log(f)
+
+
+
+
+//⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️
+//! 6: TOÁN TỬ CHUỖI
+//⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️
+
+// let firstName = 'Mai'
+// let lastName = 'Thanh Hưởng'
+
+// console.log(firstName + ' ' + lastName)
+
+
+
+//⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️
+//! 7: TOÁN TỬ SO SÁNH
+/*
+    ==                  --> Bằng
+    !=                  --> Không bằng
+    >                   --> Lớn hơn
+    <                   --> Nhỏ hơn
+    >=                   --> Lớn hơn hoặc bằng
+    <=                   --> Nhỏ hơn hoặc bằng
+*/
+//⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️
+//? ∅∅∅∅∅∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∅∅∅∅∅
+// //? Toán tử bằng
+// let a = 1
+// let b = 2
+// if (a == b) {
+//     console.log('Bằng nhau')
+// } else {
+//     console.log('Khác nhau')
+// }
+
+
+//? ∅∅∅∅∅∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∅∅∅∅∅
+// //? Toán tử không bằng
+// let c = 1
+// let d = 2
+// if (c != d) {
+//     console.log('Điều kiện đúng')
+// } else {
+//     console.log('Điều kiện sai')
+// }
+
+
+//? ∅∅∅∅∅∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∅∅∅∅∅
+// //? Toán tử lớn hơn
+// let e = 3
+// let f = 1
+// if (e > f) {
+//     console.log('E lớn hơn F')
+// } else {
+//     console.log('E nhỏ hơn F')
+// }
+
+
+//? ∅∅∅∅∅∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∅∅∅∅∅
+// //? Toán tử nhỏ hơn
+// let g = 5
+// let h = 6
+// if (g < h) {
+//     console.log('G nhỏ hơn H')
+// } else {
+//     console.log('G lớn hơn H')
+// }
+
+
+//? ∅∅∅∅∅∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∅∅∅∅∅
+// //? Toán tử nhỏ hơn hoặc bằng
+// let i = 4
+// let j = 7
+// if (i <= j) {
+//     console.log('I nhỏ hơn hoặc bằng J')
+// }
+// else {
+//     console.log('I lớn hơn hoặc bằng J')
+// }
+
+
+//? ∅∅∅∅∅∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∅∅∅∅∅
+// //? Toán tử lớn hơn hoặc bằng
+// k = 9
+// l = 2
+// if (k <= l) {
+//     console.log('K nhỏ hơn hoặc bằng L')
+// }
+// else {
+//     console.log('K lớn hơn hoặc bằng L')
+// }
+
+
+//⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️
+//! 8: BOOLEAN
+//⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️
+//? ∅∅∅∅∅∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∅∅∅∅∅
+// //? Ví dụ
+
+// var a = 1
+// var b = 2
+
+// var isSuccess = a > b
+// console.log(isSuccess)
+
+
+//? ∅∅∅∅∅∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∅∅∅∅∅
+// //? Ví dụ
+
+// let c = 5;
+// let d = 10;
+
+// let isGreater = c > d; // false
+// console.log(isGreater)
+// let isLess = c < d; // true
+// console.log(isLess)
+// let isEqual = c === d; // false
+// console.log(isEqual)
+
+
+//? ∅∅∅∅∅∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∅∅∅∅∅
+// //? Ví dụ
+
+// let str = "Hello, World!";
+// let containsHello = str.includes("Hello"); // true
+// console.log(containsHello)
+
+// let arr = [1, 2, 3];
+// let isArray = Array.isArray(arr); // true
+// console.log(isArray)
+
+
+
+
+//⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️
+//! 9: If - Else
+//⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️
+//? ∅∅∅∅∅∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∅∅∅∅∅
+//? Ví dụ 1:
+
+// let country = 'VN'
+// let age = 25
+
+// if (age >= 18 && country == 'VN') {
+//     console.log("You can get a driver's license")
+// } else {
+//     console.log("You cannot get driver's license")
+// }
+
+
+//? ∅∅∅∅∅∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∅∅∅∅∅
+//? Ví dụ 2:
+
+// let age = 18
+
+// if (age >= 18) {
+//     console.log("Bạn trưởng thành")
+// } else if (age >= 16) {
+//     console.log("Bạn là trẻ vị thành niên")
+// } else {
+//     console.log("Bạn là con nít")
+// }
+
+
+
+//⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️
+//! 10: SWITCH - CASE
+//⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️
+//? ∅∅∅∅∅∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∅∅∅∅∅
+//? Ví dụ 1
+
+// let value = "Hello"
+
+// switch (typeof value) {
+//     case "number":
+//         console.log("Type of: Number")
+//         break;
+//     case "string":
+//         console.log("Type of: String")
+//         break;
+//     case "boolean":
+//         console.log("Type of: Boolean")
+//         break;
+//     case "symbol":
+//         console.log("Type of: Symbol")
+//         break;
+//     case "undefined":
+//         console.log("Type of: Undefined")
+//         break;
+//     default:
+//         console.log("Type of: Other")
+//         break;
+// }
+
+
+
+//? ∅∅∅∅∅∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∅∅∅∅∅
+//? Ví dụ 2
+
+// let dayName = 2
+
+// switch (dayName) {
+//     case 1:
+//         dayName = "Sunday"
+//         break;
+//     case 2:
+//         dayName = "Monday"
+//         break;
+//     case 3:
+//         dayName = "Tuesday"
+//         break;
+//     case 4:
+//         dayName = "Wednesday"
+//         break;
+//     case 5:
+//         dayName = "Thursday"
+//         break;
+//     case 6:
+//         dayName = "Friday"
+//         break;
+//     case 7:
+//         dayName = "Saturday"
+//         break;
+//     default:
+//         dayName = "Invalid day number"
+//         break;
+// }
+// console.log("Today is: " + dayName)
+
+
+//? ∅∅∅∅∅∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∅∅∅∅∅
+//? Ví dụ 3
+
+// var season = "ha";
+// switch (season) {
+//     case "xuan":
+//         document.write("Mùa Xuân");
+//         break;
+//     case "ha":
+//         document.write("Mùa Hạ");
+//         break;
+//     case "thu":
+//         document.write("Mùa Thu");
+//         break;
+//     case "dong":
+//         document.write("Mùa Đông");
+//         break;
+// }
+
+//⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️
+//! 11: VIẾT ĐIỀU KIỆN RÚT GỌN
+//?  Điều kiện ? giá trị nếu điều kiện đúng ? giá trị nếu điều kiện sai
+//⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️
+//? ∅∅∅∅∅∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∅∅∅∅∅
+//? Ví dụ
+
+// let age = 20
+// let msg = (age >= 18) ? "You are an adult" : "You are a minor"
+// console.log(msg)
+
+
+//? ∅∅∅∅∅∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∅∅∅∅∅
+//? Ví dụ
+
+// let country = "Việt Nam"
+// let age = 18
+
+// let driverClient = (country = "Việt Nam", age >= 18) ? "Bạn đủ điều kiện thi bằng lái" : "Bạn chưa đủ điều kiện thi bằng lái"
+
+// console.log(driverClient)
+
+
+//⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️
+//! 11: VÒNG LẶP FOR
+//⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️
+//? ∅∅∅∅∅∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∅∅∅∅∅
+//? Ví dụ vòng lặp 10 lần
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(i)
+// }
+
+
+
+//? ∅∅∅∅∅∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∅∅∅∅∅
+//? Ví dụ lặp trong mảng
+
+// let coding = [
+//     'JS',
+//     'Python',
+//     'Java',
+// ]
+
+// for (let j = 0; j < coding.length; j++) {
+//     console.log(coding[j])
+
+// }
+
+
+
+//? ∅∅∅∅∅∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∅∅∅∅∅
+//? Ví dụ lặp trong lặp
+
+// for (let k = 0; k <= 5; k++) {
+//     console.log(k)
+//     for (let g = 0; g <= 3; g++) {
+//         console.log("Inner loop " + k)
+//     }
+// }
+
+
+
+//⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️`⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️
+//! 11: VÒNG LẶP WHILE
+//⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️
+//? ∅∅∅∅∅∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∅∅∅∅∅
+//? Ví dụ
+
+// let i = 0
+// while (i <= 10) {
+//     console.log(i)
+//     i++
+// }
+
+
+
+//? ∅∅∅∅∅∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∅∅∅∅∅
+//? Ví dụ
+// let i = 1;
+
+// do {
+//     console.log(i)
+//     i++
+// } while (i <= 5);
+
+
+
+//⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️`⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️
+//! 11: FUNCTION TRONG JAVASCRIPT
+//⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️
+//? ∅∅∅∅∅∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∅∅∅∅∅
+//? Ví dụ
+
+// function greet(name) {
+//     console.log("Xin chào, " + name + "!");
+// }
+// // Gọi function
+// greet("John"); // Xuất ra màn hình "Xin chào, John!"
+// greet("Jane"); // Xuất ra màn hình "Xin chào, Jane!"
+
+
+//? ∅∅∅∅∅∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∅∅∅∅∅
+//? Ví dụ
+
+// function greet(firstName, lastName) {
+//     document.write("Hello " + firstName + " " + lastName)
+// }
+// greet("Mai", "Thanh Hưởng");
+
+
+//? ∅∅∅∅∅∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∅∅∅∅∅
+//? Ví dụ
+
+// function sum(x, y) {
+//     document.write(x + y)
+// }
+// sum(10, 15)
+
+
+//? ∅∅∅∅∅∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∅∅∅∅∅
+//? Ví dụ
+
+// function add(a, b) {
+//     return a + b
+// }
+// let result = add(5, 6)
+// document.write("The sum is: " + result)
+
+
+//? ∅∅∅∅∅∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∅∅∅∅∅
+//? Ví dụ
+
+// function function1(x) {
+//     function function2(y) {
+//         return x * y
+//     }
+//     return function2
+// }
+// let result = function1(3)
+// document.write(result(2))
+
+
+//? ∅∅∅∅∅∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∅∅∅∅∅
+//? Ví dụ
+
+// function celsiusToFahrenheit(celsius) {
+//     return (celsius * 9) / 5 + 32;
+// }
+// document.write(celsiusToFahrenheit(25)); // Output: 77
+
+
+//? ∅∅∅∅∅∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∅∅∅∅∅
+//? Ví dụ
+
+// function tongMang(numbers) {
+//     let tong = 0
+//     for (let i = 0; i < numbers.length; i++) {
+//         tong += numbers[i]
+//     }
+//     return tong
+// }
+
+// let nums = [1, 2, 3, 4, 5]
+// document.write(tongMang(nums))
+
+
+
+//⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️`⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️
+//! 11: CALLBACK TRONG JAVASCRIPT: Callback là một hàm (function) truyền vào cho một hàm khác để thực thi sau khi hoàn thành các công việc đã biết trước.
+//⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️⚙️
+//? ∅∅∅∅∅∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∅∅∅∅∅
+//? Ví dụ
+
+// function display(result) {
+//     document.write(result)
+// }
+// function add(num1, num2, myCallback) {
+//     let sum = num1 + num2
+//     myCallback(sum)
+// }
+// add(10, 20, display)
+
+
+//? ∅∅∅∅∅∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∞∅∅∅∅∅
+//? Ví dụ
+
+
+
 
