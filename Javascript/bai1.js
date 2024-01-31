a3dac3d58e5605a4e78c8b5d9e67f0781b73f820
HuongMT98 <134026872+HuongMT98@users.noreply.github.com>
2024-01-31 11:53:47 +0700

    bai1

---
+++ Javascript/bai1.js
@@ -0,0 +1,60 @@
+//!               1: Biến
+/*  Khai báo biến: Bạn có thể khai báo biến trong JavaScript bằng từ khóa var, let hoặc const. Ví dụ:
+
+                var number = 10;
+                let name = "John";
+                const PI = 3.14;            -> Hằng số
+
+*/
+//1️⃣1️⃣1️⃣1️⃣1️⃣1️⃣1️⃣1️⃣1️⃣1️⃣1️⃣1️⃣1️⃣1️⃣1️⃣1️⃣1️⃣1️⃣
+// console.log("           Bài 1: Biến")
+
+
+// var tenHocSinh = 'Nguyễn Văn A'
+// let diemTrungBinh = 8.2
+// const danhHieu = "học sinh giỏi"
+// console.log("Học Sinh:", tenHocSinh, "có điểm trung bình:", diemTrungBinh, "và đạt danh hiệu", danhHieu)
+
+
+
+
+
+//🎟️🎟️🎟️🎟️🎟️🎟️🎟️🎟️🎟️🎟️🎟️🎟️🎟️🎟️🎟️🎟️🎟️🎟️🎟️🎟️🎟️🎟️🎟️🎟️🎟️🎟️🎟️🎟️🎟️
+//🎫🎫🎫🎫🎫🎫🎫🎫🎫🎫🎫🎫🎫🎫🎫🎫🎫🎫🎫🎫🎫🎫🎫🎫🎫🎫🎫🎫🎫
+
+//!               2: Kiểu dữ liệu
+/* //* Trong JavaScript, có một số kiểu dữ liệu cơ bản như sau:
+
+//* Kiểu dữ liệu nguyên thủy (Primitive data types):
+
+            Số (Number): Ví dụ: let num = 10;
+            Chuỗi (String): Ví dụ: let str = "Hello World";
+            Boolean: Ví dụ: let isTrue = true;
+
+//* Kiểu dữ liệu phức tạp (Complex data types):
+
+            Mảng (Array): Ví dụ: let arr = [1, 2, 3];
+            Đối tượng (Object): Ví dụ: let obj = { name: "John", age: 30 };
+
+
+//* Kiểu dữ liệu đặc biệt:
+
+            Null: Ví dụ: let nullValue = null;
+            Không xác định (Undefined): Ví dụ: let undefinedValue;
+
+*/
+//1️⃣1️⃣1️⃣1️⃣1️⃣1️⃣1️⃣1️⃣1️⃣1️⃣1️⃣1️⃣1️⃣1️⃣1️⃣1️⃣1️⃣1️⃣
+console.log("           Bài 2: Kiểu dữ liệu")
+
+
+
+
+
+
+
+
+
+
+
+//🎟️🎟️🎟️🎟️🎟️🎟️🎟️🎟️🎟️🎟️🎟️🎟️🎟️🎟️🎟️🎟️🎟️🎟️🎟️🎟️🎟️🎟️🎟️🎟️🎟️🎟️🎟️🎟️🎟️
+//🎫🎫🎫🎫🎫🎫🎫🎫🎫🎫🎫🎫🎫🎫🎫🎫🎫🎫🎫🎫🎫🎫🎫🎫🎫🎫🎫🎫🎫
