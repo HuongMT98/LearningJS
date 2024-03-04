d1f2628fcbb097849c9ede91b6cf51dd8ea8605e
HuongMT98 <134026872+HuongMT98@users.noreply.github.com>
2024-03-04 15:24:22 +0700

    Added a new div element with class "box" and added an h2 element inside it. (generated by blackbox.ai )

--- Javascript/DOM.js
+++ Javascript/DOM.js
@@ -3,3 +3,8 @@ let paragraphElement = document.querySelector('p');
 
 console.log(headingElement.innerText)
 
+
+
+
+let boxElement = document.querySelector('.box')
+boxElement.innerHTML = '<h2>Thêm h2 vào  div</h2>'
